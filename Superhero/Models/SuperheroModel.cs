e0ad83a1ed910675a3ea137eb0b332592f70af57
victor-meza-07 <victord.meza@gmail.com>
2020-03-17 15:29:46 -0500

    added: Superhero Model

---
+++ Superhero/Models/SuperheroModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Superhero.Models
+{
+    public class SuperheroModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AlterEgo { get; set; }
+        public string Primary_Ability { get; set; }
+        public string Secondary_Ability { get; set; }
+        public string Catch_Phrase { get; set; }
+    }
+}
